0c0b2d52f9e2aed964d04e80dab474579a4cb392
pmusaraj <pmusaraj@gmail.com>
2018-01-27 22:17:36 -0500

    add missing include for CustomWebView for android

--- android/app/src/main/java/com/discosingle/MainApplication.java
+++ android/app/src/main/java/com/discosingle/MainApplication.java
@@ -27,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new CustomWebViewPackage(),
             new ReactNativeOneSignalPackage(),
             new RNKeyPairPackage(),
             new RNDeviceInfo(),
